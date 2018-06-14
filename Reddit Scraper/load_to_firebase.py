f68fdabe8c1702fef151ac5b207da18ca2d50e14
Hasan Zahid <hasanthedev@gmail.com>
2018-06-14 17:19:57 -0500

    Modified multiple folder support for firebase

--- Reddit Scraper/load_to_firebase.py
+++ Reddit Scraper/load_to_firebase.py
@@ -1,4 +1,4 @@
-import pyrebase, firebase_config, os, datetime
+import pyrebase, firebase_config, os, datetime, random
 
 config = {
   "apiKey": firebase_config.api_key,
@@ -9,21 +9,35 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 
-def upload_to_storage_db():
-        
-    dir = "C:\\Users\\wazih\\Desktop\\courses\\Images"
+# generates a key for firebase
+letters_for_key = '!abcdefghijklmanopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+def key_gen(key_length):
+  str = ""
+  counter = 0
+  
+  while counter < key_length:
+    rand = random.randint(1, len(letters_for_key)-1)  
+    str = str + letters_for_key[rand]
 
-    now = datetime.datetime.now()
-    folder = now.strftime('%m_%d_%Y')
+    counter = counter + 1
 
-    # init firebase vars
-    db = firebase.database()
-    storage = firebase.storage()
+  return str  
+  
+
+def upload_to_storage_db():   
+  dir = "C:\\Users\\wazih\\Desktop\\courses\\Images"
+  folders = os.listdir(dir)
+
+  # init firebase vars
+  db = firebase.database()
+  storage = firebase.storage()
 
 
-    # prepping for image counting and getting the length of db for db count
-    db_len = 0
+  # prepping for image counting and getting the length of db for db count
+  db_len = 0
+  next_folder_count = 0
 
+  for folder in folders:
     try:
         images_from_db = db.child(folder).get()
         db_len = len(images_from_db.val())
@@ -32,7 +46,7 @@ def upload_to_storage_db():
 
     for i in range(len(os.listdir(dir + "\\" + folder))):
         # storing to firebase
-        upload_image = storage.child(folder + '/' + str(i) + '.jpg').put(dir + '\\' + folder + '\\' + str(i) + '.jpg')
+        upload_image = storage.child(folder + '/' + str(i) + '.jpg').put(dir + '\\' + folder + '\\' + str(i+next_folder_count) + '.jpg')
         # printing a tracking message
         print('File uploaded', i+1, '/', len(os.listdir(dir + "\\" + folder)))
 
@@ -40,8 +54,11 @@ def upload_to_storage_db():
         data_url = storage.child(folder + '/' + str(i) + '.jpg').get_url(upload_image['downloadTokens'])
 
         # prepping the data to upload to the database with the download link from storage
-        data = {str(db_len + i) : data_url}
+        key = key_gen(4)
+        data = {key : data_url}
 
         db.child(folder).update(data)
 
-    print("Finished uploading to firebase")    
+    next_folder_count = next_folder_count + 100
+
+  print("Finished uploading to firebase")    
