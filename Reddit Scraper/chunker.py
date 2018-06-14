2248367932dc29fe963dc0805230cc8f866b326b
Hasan Zahid <hasanthedev@gmail.com>
2018-06-14 16:32:55 -0500

    Modified chunker for Reddit Scraper support

--- Reddit Scraper/chunker.py
+++ Reddit Scraper/chunker.py
@@ -1,41 +1,49 @@
 import os, datetime, math, shutil, sys
 
 
-# for Reddit scraper 
-# now = datetime.datetime.now()
-# root_dir = now.strftime("%m_%d_%Y")
-
-
 def chunker_mech(chunk_by, path_to_root):
   now = datetime.datetime.now()
-  root = now.strftime("%m_%d_%Y")
+  root = path_to_root + '\\' + now.strftime("%m_%d_%Y")
 
   num_of_files = len(os.listdir(root))
 
-  num_folders_to_make = math.ceil(num_of_files / chunk_by)
+  # num_folders_to_make = 0
+  if num_of_files > 100:
+    # deciding how mnay folders to make based on if number of files is odd or even
+    # if num_of_files % 2 == 0:
+    #   num_folders_to_make = math.floor(num_of_files / chunk_by)
+    # else:
+    #   num_folders_to_make = math.ceil(num_of_files / chunk_by)
 
-  # creates all the empty folkders to hold the image chunks
-  for i in range(num_folders_to_make):
-    folder_name = root + "_" + str(i+1)
-    try:
-      os.makedirs(folder_name)   
-    except FileExistsError:
-      print("Folder already made and exists")  
-  print("\nFolders have been made")   
+    num_folders_to_make = math.floor(num_of_files / chunk_by)
 
-
-  counter = 0
-  # looping over the amoung of folders
-  for i in range(num_folders_to_make):
-    print("Chunking to ", root + "_" + str(i)) 
-    # each time I loop over a folder I copy the files  
-    for f in range(counter, counter+chunk_by):
+    # creates all the empty folkders to hold the image chunks
+    for i in range(num_folders_to_make):
+      folder_name = root + "_" + str(i+1)
       try:
-        # copies file to destination and overwrites if the file already exists in destination
-        shutil.copy(root + '\\' + str(f) + ".png", root + "_" + str(i+1))
-      except FileNotFoundError:
-        print("\n\nChuncking finished")
-        break
-    counter = counter + chunk_by  
+        os.makedirs(folder_name)   
+      except FileExistsError:
+        print("Folder already made and exists")  
+    print("\nFolders have been made")   
 
+
+    counter = 0
+    # looping over the amoung of folders
+    for i in range(num_folders_to_make):
+      print("Chunking to ", root + "_" + str(i)) 
+      # each time I loop over a folder I copy the files  
+      for f in range(counter, counter+chunk_by):
+        try:
+          # copies file to destination and overwrites if the file already exists in destination
+          shutil.copy(root + '\\' + str(f) + ".jpg", root + "_" + str(i+1))
+        except FileNotFoundError:
+          print("\n\nChuncking finished")   
+          break
+      counter = counter + chunk_by
+
+    #removing the root dir to avoid overlap
+    print("removing ", root)
+    shutil.rmtree(root)    
+  else:
+    print("No need for chunking")   
 
--- Reddit Scraper/scraper_dl_demo.py
+++ Reddit Scraper/scraper_dl_demo.py
@@ -1,8 +1,14 @@
-import praw_config, praw, re, os
+import praw_config, praw, re, os, datetime
 from dlr_helper import scrape_album_links, download_pics
 from load_to_firebase import upload_to_storage_db
+from chunker import chunker_mech
 
+
+# # init vals to help chunking the root folder
 dir = "C:\\Users\\wazih\\Desktop\\courses\\Images"
+# now = datetime.datetime.now()
+# root_folder = now.strftime('%m_%d_%Y')
+
 
 reddit = praw.Reddit(client_id=praw_config.client_id, 
                      client_secret=praw_config.client_secret, 
@@ -35,6 +41,8 @@ scrape_album_links(album_urls, "links")
 
 print("\n now chuncking...")
 
-# print("\nuploading to firebase")
+chunker_mech(100, dir)
 
-# upload_to_storage_db()
+print("\nuploading to firebase")
+
+upload_to_storage_db()
