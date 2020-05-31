e03724af1d7cb7794bab97bc5e35ec4a49163d96
Hasan Zahid <hasanthedev@gmail.com>
2020-05-30 22:31:00 -0500

    new links

--- Job Scraper/glassdoor.py
+++ Job Scraper/glassdoor.py
@@ -1,4 +1,3 @@
-from selenium import webdriver
 import time
 import pymongo
 from bson import ObjectId
@@ -14,10 +13,10 @@ mydb = myclient["jobDB"]
 job_coll = mydb["jobs"]
 
 
-def get_jobs(job_dict, jobRole, joblist):
+def get_jobs(job_dict, jobType, joblist, jobRole, driver, db_links):
   # this counter is to avoid duplicate keys because of same companies posting different positions
   company_counter = 0
-  for job in joblist:
+  for job in joblist[:5]:
     try:
       # each job element gets clicked
       job.click() 
@@ -33,13 +32,15 @@ def get_jobs(job_dict, jobRole, joblist):
     # elements
     company_element = driver.find_element_by_class_name("employerName")
     title_element = driver.find_element_by_class_name("title")
-    # this try catch for the time when the job has expired and the apply now button does not exist
+    # this try catch for the time when the 'job has expired' and the apply now button does not exist
     try:
       apply_element = driver.find_element_by_class_name("applyButton")
       link_name = apply_element.text
       apply_link = ""
     except NoSuchElementException:
       print("THE JOB HAS EXPRED")
+      # if the 'job has expired' then the apply link & link name is:
+      link_name = ""
       apply_link = "job has expired"
     
     # element manipualted
@@ -52,91 +53,111 @@ def get_jobs(job_dict, jobRole, joblist):
       # only get link if the apply now button exists
       if apply_link != "job has expired":
         apply_link = apply_element.get_attribute("href")
+        # removing the GUID so that the links can be checked again to see if applied
+        guid_removed = apply_link.split("&guid=")
+        src_removed = guid_removed[1].split("&src")[1]
+        # apply link with unique guid removed
+        apply_link = guid_removed[0] + "&src" + src_removed
+        # cs and cb id removed
+        cs_removed = apply_link.split("&cs=")
+        cb_removed = guid_removed[1].split("&cb")[1].split("&")
+        # actual apply link
+        apply_link = cs_removed[0] + "&" + cb_removed[1]
 
-      # if the key already exists, this happens when the same company posts a different position
-      if company_name in job_dict.keys():
-        company_counter+=1
-        print("Key EXISTS ", company_name)
-        # if company name exists as a key we attach a counter to the key and then put it in the dict
-        job_dict["web developer"][jobRole][company_name+str(company_counter)] = { "job link": apply_link, "job title": job_title }
+      if apply_link in db_links:
+        print("JOB HAS BEEN ALREADY APPLIED")
       else:
-        job_dict["web developer"][jobRole][company_name] = { "job link": apply_link, "job title": job_title }
+        # if the key already exists, this happens when the same company posts a different position
+        if company_name in job_dict.keys():
+          company_counter+=1
+          print("Key EXISTS ", company_name)
+          # if company name exists as a key we attach a counter to the key and then put it in the dict
+          job_dict[jobRole][jobType][company_name+str(company_counter)] = { "job link": apply_link, "job title": job_title }
+        else:
+          job_dict[jobRole][jobType][company_name] = { "job link": apply_link, "job title": job_title }
     else:
       print("Easy apply")
       if apply_link != "job has expired":
         easy_app(apply_element)
         easy_apply_link = company_name + " " + job_title
 
-      if company_name in job_dict.keys():
-        company_counter+=1
-        print("Key EXISTS ", company_name)
-        job_dict["web developer"][jobRole][company_name+str(company_counter)] = { "job link": easy_apply_link, "job title": job_title }
+      if easy_apply_link in db_links:
+        print("JOB HAS BEEN ALREADY APPLIED")
       else:
-        job_dict["web developer"][jobRole][company_name] = { "job link": easy_apply_link, "job title": job_title }
+        if company_name in job_dict.keys():
+          company_counter+=1
+          print("Key EXISTS ", company_name)
+          job_dict[jobRole][jobType][company_name+str(company_counter)] = { "job link": easy_apply_link, "job title": job_title }
+        else:
+          job_dict[jobRole][jobType][company_name] = { "job link": easy_apply_link, "job title": job_title }
   
   return job_dict
 
   
-def glassdoor_scrape(db_id, driver, roles):
+def glassdoor_scrape(db_id, driver, job_types, job_role, glassdoor_link):
+  driver.get(glassdoor_link)
   if db_id == '':
-  # this dict is to check and update the dictionary
-  job_dict = { "web developer": {} }
-  for role in roles:
-    # job type clicks 
-    job_type_elem = driver.find_element_by_xpath("//div[@id='filter_jobType']")
-    job_type_elem.click()
-    time.sleep(1)
+    # this dict is to check and update the dictionary
+    job_dict = { job_role: {} }
 
-    # click on the roles
-    driver.find_element_by_xpath("//li[@value="+role+"]").click()
-    time.sleep(3)
-    
-    # job list 
-    job_on_page = driver.find_elements_by_class_name("jl")
+    # going through each jpb type for ex: part time or internship etc.
+    for job_type in job_types:
+      # job type clicks 
+      job_type_elem = driver.find_element_by_xpath("//div[@id='filter_jobType']")
+      job_type_elem.click()
+      time.sleep(1)
 
-    # get pagination
-    try: 
-      next_page_elements = driver.find_elements_by_xpath("//li[@class='page']")
-      pages = [page for page in next_page_elements]
-      last_page = driver.find_element_by_xpath("//li[@class='page last']")  
-      pages.append(last_page)
-    except NoSuchElementException:
-      print("NO PAGINATION/ ONLY 1 PAGE")
+      # click on the roles
+      driver.find_element_by_xpath("//li[@value="+job_type+"]").click()
+      time.sleep(3)
+      
+      # job list 
+      job_on_page = driver.find_elements_by_class_name("jl")
 
-    if len(pages) > 0:
-      for page_elem in pages:
-        if not job_dict["web developer"]:
-          job_dict = { "web developer": { role: {} } }
-          job_dict = get_jobs(job_dict, role, job_on_page)
+      # get pagination
+      try: 
+        next_page_elements = driver.find_elements_by_xpath("//li[@class='page']")
+        pages = [page for page in next_page_elements]
+        last_page = driver.find_element_by_xpath("//li[@class='page last']")  
+        pages.append(last_page)
+      except NoSuchElementException:
+        print("NO PAGINATION/ ONLY 1 PAGE")
 
-          job_dict["web developer"]["'internship'"] = {}
-        elif not job_dict["web developer"]["'internship'"]:
-          jobs = get_jobs(job_dict, role, job_on_page)
-        else:
-          jobs = get_jobs(job_dict, role, job_on_page)
+      if len(pages) > 0:
+        for page_elem in pages:
+          if not job_dict[job_role]:
+            job_dict = { job_role: { job_type: {} } }
+            job_dict = get_jobs(job_dict, job_type, job_on_page, job_role, driver, [])
 
-        # after getting the jobs on that page clicking to next page
-        page_elem.click()
-        time.sleep(2)
-    else:
-      if not job_dict["web developer"]:
-        # we change the dictionary with the role 
-        job_dict = { "web developer": { role: {} } }
-        job_dict = get_jobs(job_dict, role, job_on_page)
-        # after I get the job dict back I insert the internship key
-        job_dict["web developer"]["'internship'"] = {}
-      elif not job_dict["web developer"]["'internship'"]:
-        jobs = get_jobs(job_dict, role, job_on_page)
+            job_dict[job_role]["'internship'"] = {}
+          elif not job_dict[job_role][job_type]:
+            jobs = get_jobs(job_dict, job_type, job_on_page, job_role, driver, [])
+          else:
+            jobs = get_jobs(job_dict, job_type, job_on_page, job_role, driver, [])
+
+          # after getting the jobs on that page clicking to next page
+          page_elem.click()
+          time.sleep(2)
       else:
-        jobs = get_jobs(job_dict, role, job_on_page)  
+        if not job_dict[job_role]:
+          # we change the dictionary with the role 
+          job_dict = { job_role: { job_type: {} } }
+          job_dict = get_jobs(job_dict, job_type, job_on_page, job_role, driver, [])
+          # after I get the job dict back I insert the internship key
+          job_dict[job_role]["'internship'"] = {}
+        elif not job_dict[job_role][job_type]:
+          jobs = get_jobs(job_dict, job_type, job_on_page, job_role, driver, [])
+        else:
+          jobs = get_jobs(job_dict, job_type, job_on_page, job_role, driver, [])  
 
-  job_db_id = job_coll.insert_one(jobs)
-  # writing to the file the id
-  with open("ids.txt", 'a') as outfile:
-    outfile.write(str(job_db_id.inserted_id)+"\n")
-  print("----------------------------------\nJOBS INSERTED\n----------------------------------")
-  driver.close()
+    job_db_id = job_coll.insert_one(jobs)
+    # writing to the file the id
+    with open("ids.txt", 'a') as outfile:
+      outfile.write(str(job_db_id.inserted_id)+"\n")
+    print("----------------------------------\nJOBS INSERTED\n----------------------------------")
+    driver.close()
   else:
+    # db query
     job_db_id = ObjectId(db_id)
     db_query = { "_id": job_db_id }
     db_jobs = job_coll.find(db_query)
@@ -144,12 +165,20 @@ def glassdoor_scrape(db_id, driver, roles):
     # this list is used to check if a job has been applied to
     job_links = []
     for job in db_jobs:
-      # appending the part time links to the job_links arr 
-      job_links = [job["web developer"]["'parttime'"][key]['job link'] for key in job["web developer"]["'parttime'"].keys()]
-      # then appending the internship links
-      for key in job["web developer"]["'internship'"].keys():
-        job_links.append(job["web developer"]["'internship'"][key]['job link'])
+      for job_type in job_types:
+        job_type_elem = driver.find_element_by_xpath("//div[@id='filter_jobType']")
+        job_type_elem.click()
+        time.sleep(1)
+        driver.find_element_by_xpath("//li[@value="+job_type+"]").click()
+        time.sleep(3)
+        job_on_page = driver.find_elements_by_class_name("jl")
+
+        job_dict = { job_role: { job_type: {} } }
+
+        job_links = [job[job_role][job_type][key]['job link'] for key in job[job_role][job_type].keys()]
+        
+        jobs = get_jobs(job_dict, job_type, job_on_page, job_role, driver, job_links)
+        print(job_links, "\n\n")
+        print(jobs)
 
     driver.close()
-
-
--- Job Scraper/main.py
+++ Job Scraper/main.py
@@ -1,11 +1,17 @@
-# job roles I am looking for
-roles = ["'parttime'", "'internship'"]
+from selenium import webdriver
+from glassdoor import glassdoor_scrape
 
-# selenium driver
-driver = webdriver.Chrome()
-driver.get("https://www.glassdoor.com/Job/ashburn-web-developer-jobs-SRCH_IL.0,7_IC1130338_KO8,21.htm")
+# job roles I am looking for
+job_types = ["'parttime'", "'internship'"]
 
 with open("ids.txt", 'r') as infile:
   mongo_id = infile.read()
   ids = mongo_id.split("\n")
 print("id", ids)
+
+# selenium driver
+driver1 = webdriver.Chrome()
+web_id = ids[0]
+
+glassdoor_scrape(web_id, driver1, job_types, 
+                 "web developer", "https://www.glassdoor.com/Job/ashburn-web-developer-jobs-SRCH_IL.0,7_IC1130338_KO8,21.htm")
